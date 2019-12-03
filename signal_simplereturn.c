f5697fececdad7eee095b6bd2738e01d29deae43
parlos <patrik.arlos@gmail.com>
2019-12-03 09:14:14 +0100

    Modified so that multiple signals can be caught.

--- signal_simplereturn.c
+++ signal_simplereturn.c
@@ -6,13 +6,24 @@
 volatile sig_atomic_t keep_going = 1;
 
 /* The signal handler just clears the flag and re-enables itself. */
-void
-catch_alarm (int sig)
+void catch_alarm (int sig)
 {
   puts("Got ALARM!\n");
   keep_going = 0;
-  signal (sig, catch_alarm);
+  //  signal (sig, catch_alarm);
 }
+
+
+int intrcnt=0;
+void catch_int (int sig)
+{
+  printf("Got INT %d!\n", intrcnt);
+  intrcnt++;
+  //  signal (sig, catch_alarm);
+}
+
+
+
 
 void
 do_stuff (void)
@@ -25,13 +36,15 @@ main (void)
 {
   /* Establish a handler for SIGALRM signals. */
   signal (SIGALRM, catch_alarm);
+  signal (SIGINT, catch_int);
 
   /* Set an alarm to go off in a little while. */
-  alarm (2);
+  alarm (20);
 
   /* Check the flag once in a while to see when to quit. */
-  while (keep_going)
+  while (keep_going){
     do_stuff ();
-
+    sleep(1);
+  }
   return EXIT_SUCCESS;
 }
